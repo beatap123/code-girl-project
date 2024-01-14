28b469bfc462b2837d16ee36843a893e1ab3a57e
beata <putkowska.b@gmail.com>
2024-01-14 10:52:53 +0100

    commit

--- code.js
+++ code.js
@@ -1,27 +1,14 @@
-const myButton = document.querySelector(".button")
+/*const myButton = document.querySelector(".button")
 
 function sayHello() {
   console.log("No to Hello!")
 }
 
-myButton.addEventListener("click", sayHello);
-
+myButton.addEventListener("click", sayHello);*/
 const button = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav");
 
-function mobileMenu() {
-    if (window.innerWidth <= 800) {
-        nav.classList.add("nav-mobile");
-        button.classList.add("menu-button-open");
-        button.innerHTML = "X";
-    } else {
-        nav.classList.remove("nav-mobile");
-        button.classList.remove("menu-button-open");
-        button.innerHTML = "menu";
-    }
-}
-
-/*function toggleMenu() {
+function toggleMenu() {
   nav.classList.toggle("nav-mobile");
   button.classList.toggle("menu-button-open");
 
@@ -30,6 +17,6 @@ function mobileMenu() {
   } else {
     button.innerHTML = "menu";
   }
-}*/
+}
 
-button.addEventListener("click", mobileMenu);
+button.addEventListener("click", toggleMenu);
