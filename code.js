387aaeb0cf4e7510d04c507815a4fc543213edc2
beata <putkowska.b@gmail.com>
2024-01-14 12:30:01 +0100

    change mobile menu

--- code.js
+++ code.js
@@ -9,14 +9,14 @@ const button = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav");
 
 function toggleMenu() {
-  nav.classList.toggle("nav-mobile");
-  button.classList.toggle("open");
-
-  if (button.classList.contains("open")) {
-    button.innerHTML = "X";
-  } else {
-    button.classList.remove("open");
+    nav.classList.toggle("nav-mobile");
+    button.classList.toggle("open");
+  
+    if (button.classList.contains("open")) {
+      button.innerHTML = "X"; // Change this to whatever you want the button to display when the menu is open
+    } else {
+      button.innerHTML = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M4 5h16v2h-16v-2zm0 6h16v2h-16v-2zm0 6h16v2h-16v-2z"/></svg>`; // Replace this with your desired SVG
+    }
   }
-}
 
 button.addEventListener("click", toggleMenu);
