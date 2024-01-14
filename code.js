5766c492ac60dd60461f49862f58622caba68597
beata <putkowska.b@gmail.com>
2024-01-14 10:57:31 +0100

    changebutton

--- code.js
+++ code.js
@@ -10,12 +10,12 @@ const nav = document.querySelector(".nav");
 
 function toggleMenu() {
   nav.classList.toggle("nav-mobile");
-  button.classList.toggle("menu-button-open");
+  button.classList.toggle("open");
 
   if (button.classList.contains("menu-button-open")) {
     button.innerHTML = "X";
   } else {
-    button.innerHTML = "menu";
+    button.classList.remove("open");
   }
 }
 
