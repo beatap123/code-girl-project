cb0c6b6347ce43af8744277481f59c6bed2e0b0d
beata <putkowska.b@gmail.com>
2024-01-14 12:32:18 +0100

    change svg

--- code.js
+++ code.js
@@ -15,7 +15,7 @@ function toggleMenu() {
     if (button.classList.contains("open")) {
       button.innerHTML = "X"; // Change this to whatever you want the button to display when the menu is open
     } else {
-      button.innerHTML = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M4 5h16v2h-16v-2zm0 6h16v2h-16v-2zm0 6h16v2h-16v-2z"/></svg>`; // Replace this with your desired SVG
+      button.innerHTML = `<svg width="52px" height="52px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#ffffff"><path d="M3 5H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 12H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 19H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>`; // Replace this with your desired SVG
     }
   }
 
